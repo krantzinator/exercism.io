8496f6462a4dd74cc7d2b390edb905d3eebd3547
krantzinator <krantzrs8@gmail.com>
2016-09-14 15:21:06 -0400

    first refactor for readability

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,11 +1,15 @@
 var Gigasecond = function(startDate) {
-  this.startDate = startDate;
+  this.startDate = startDate.getTime();
 };
 
 // A gigasecond is one billion (10**9) seconds.
 
 Gigasecond.prototype.date = function() {
-  var resultDate = this.startDate.getTime() + (Math.pow(10, 9) * 1000);
+  var gigasecond = Math.pow(10,9);
+  var milisecondsToSeconds = 1000;
+
+  var resultDate = this.startDate + (gigasecond * milisecondsToSeconds);
+
   return new Date(resultDate);
 };
 
