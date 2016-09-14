eac9c3f47070f789f55652a4348d71ab791e747e
krantzinator <krantzrs8@gmail.com>
2016-09-14 15:15:07 -0400

    first run through at passing tests; JS Gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,5 +1,12 @@
-var Gigasecond = function() {};
+var Gigasecond = function(startDate) {
+  this.startDate = startDate;
+};
 
+// A gigasecond is one billion (10**9) seconds.
 
+Gigasecond.prototype.date = function() {
+  var resultDate = this.startDate.getTime() + (Math.pow(10, 9) * 1000);
+  return new Date(resultDate);
+};
 
 module.exports = Gigasecond;
