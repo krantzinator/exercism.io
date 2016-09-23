1860b40a87628a44df57ab51a77817569519c387
krantzinator <krantzrs8@gmail.com>
2016-09-23 10:43:22 -0400

    first pass at JS Word Count

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,23 @@
+var Words = function() {};
+
+Words.prototype.count = function(phrase) {
+  var phrase_array = phrase.trim().split(/\s+|\n+|\t+/);
+  var result = {};
+
+  phrase_array.forEach(function(word) {
+    word_to_find = word.toLowerCase();
+    var word_count = 0;
+    for (var i = 0; i < phrase_array.length; i++) {
+      if (phrase_array[i].toLowerCase() === word_to_find) {
+        word_count += 1;
+        console.log(word_to_find);
+      }
+    }
+    result[word_to_find] = word_count;
+  });
+  return result;
+  // increase counter, then place in new key:value hash
+  // before counting new words make sure it's not already in the result hash
+};
+
+module.exports = Words;
