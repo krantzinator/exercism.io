59c8bae30ca93775b933d330c85fcd04f33704c8
krantzinator <krantzrs8@gmail.com>
2017-08-31 10:53:56 -0400

    isogram first test

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,16 +1,16 @@
 var Words = function() {};
 
 Words.prototype.count = function(phrase) {
-  var phrase_array = phrase.trim().split(/\s+|\n+|\t+/);
+  var phrase_array = phrase.trim().split(/\s+/);
   var result = {};
 
   phrase_array.forEach(function(word) {
     word_to_find = word.toLowerCase();
     var word_count = 0;
+    // don't need to double-loop
     for (var i = 0; i < phrase_array.length; i++) {
       if (phrase_array[i].toLowerCase() === word_to_find) {
         word_count += 1;
-        console.log(word_to_find);
       }
     }
     result[word_to_find] = word_count;
