19011505c4472ec62bdca9fa1550d3b3b41466da
krantzinator <krantzrs8@gmail.com>
2017-08-31 11:10:44 -0400

    single word string handled

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,8 @@
 def is_isogram(string):
+    string = string.lower()
+    for x in range(len(string)):
+        count = string.count(string[x])
+        if count > 1:
+            return False
+        continue
     return True
